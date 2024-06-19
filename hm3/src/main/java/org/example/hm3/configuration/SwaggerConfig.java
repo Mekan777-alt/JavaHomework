681455da41d6abe0394eb62539dc019cccea0937
Mekan777-alt <mekan2678@gmail.com>
2024-06-20 00:19:47 +0300

    update OpenAPI and add Annotated Operation

---
+++ hm3/src/main/java/org/example/hm3/configuration/SwaggerConfig.java
@@ -0,0 +1,16 @@
+package org.example.hm3.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Домашка по JAVA №3").version("1.0.0").contact(new Contact()));
+    }
+}
--- hm3/src/main/java/org/example/hm3/controllers/ProductController.java
+++ hm3/src/main/java/org/example/hm3/controllers/ProductController.java
@@ -1,4 +1,6 @@
 package org.example.hm3.controllers;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.example.hm3.database.models.Product;
 import org.example.hm3.models.dto.ProductDTO;
 import org.example.hm3.service.ProductService;
@@ -9,30 +11,36 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
 @RestController
+@Tag(name = "CRUD по продуктам")
 public class ProductController {
 
     @Autowired
     private ProductService productService;
 
     @GetMapping("/product")
+    @Operation(summary = "Возвращает массив продуктов")
     public ResponseEntity<List<Product>> product() {
         return ResponseEntity.ok(productService.getProducts());
     }
 
     @PostMapping("/product")
+    @Operation(summary = "Создает продукт")
     public ResponseEntity<Product> addProduct(@RequestBody ProductDTO product) {
         Product newProduct = productService.insertProduct(product);
         return ResponseEntity.status(HttpStatus.CREATED).body(newProduct);
     }
 
     @PutMapping("/product/{article_id}")
+    @Operation(summary = "Обновляет продукт по id")
     public ResponseEntity<Product> updateProduct(@PathVariable int article_id, @RequestBody ProductDTO product) {
         Product updateProduct = productService.updateProduct(product, article_id);
         return ResponseEntity.status(HttpStatus.OK).body(updateProduct);
     }
 
     @DeleteMapping("/product/{article_id}")
+    @Operation(summary = "Удаляет продукт по id")
     public ResponseEntity<String> deleteProduct(@PathVariable int article_id) {
         boolean deleteProduct = productService.deleteProduct(article_id);
         if (deleteProduct) {
